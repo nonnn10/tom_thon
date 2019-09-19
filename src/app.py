f1aff91c0ff6ba60031d8eaccce359071ff38847
Shohei_Tamashiro <33239458+shoheifate@users.noreply.github.com>
2019-09-19 22:23:38 +0900

    update app.py

--- src/app.py
+++ src/app.py
@@ -72,3 +72,17 @@ def classify(model):#分類結果を返す関数
     return y, proba                #戻り値として予測したラベル、確率を返す
 
 print(classify(forest))
+
+
+
+text = classify(lr)
+#print(text)
+
+@app.route('/')
+def home():
+    return render_template('index.html',text = text)
+          
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
